37ba40b9f4056d94ab642daa9129e8b941430cac
coolepoff <97253599+coolepoff@users.noreply.github.com>
2022-06-16 14:27:48 +0300

    module4-task1

--- js/main.js
+++ js/main.js
@@ -14,6 +14,23 @@ const NAMES = [
 ];
 
 const PICTURE_COUNT = 25;
+const COMMENT_COUNT = 5;
+
+const LikesCount = {
+  min: 15,
+  max: 200
+};
+
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const MESSAGE_COUNT = 2;
 
 function getRandomNumber(min, max) {
   if (min > max) {
@@ -28,22 +45,36 @@ function getRandomNumber(min, max) {
 function checkLenght(checkString, maxlegth) {
   return checkString.length < maxlegth;
 }
-
 checkLenght('Проверяемая строка', 20);
 
 const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
 
+let indexID = 0;
+let indexChildID = 0;
+function createId() {
+  indexID = indexID + 1;
+  return indexID;
+}
+
+function createChildId() {
+  indexChildID = indexChildID + 1;
+  return indexChildID;
+}
+
+const createComment = () => ({
+  id: createChildId(),
+  avatar: `img/avatar-${getRandomNumber(1, 6)}.svg`,
+  message: getRandomArrayElement(MESSAGES) + ' ' + getRandomArrayElement(MESSAGES),
+  name: getRandomArrayElement(NAMES),
+});
+
+
 const createPicture = () => ({
-  id: getRandomNumber (1, PICTURE_COUNT),
-  url: `photos/${getRandomNumber (1, PICTURE_COUNT)}.jpg`,
-  description: `Описание фотографии ${getRandomNumber (1, PICTURE_COUNT)}`,
-  likes: '',
-  comments: {
-    id: '',
-    avatar: `img/avatar-${getRandomNumber (1, 6)}.svg`,
-    message: '',
-    name: getRandomArrayElement(NAMES),
-  },
+  id: createId(),
+  url: `photos/${indexID}.jpg`,
+  description: `Описание фотографии ${indexID}`,
+  likes: getRandomNumber (LikesCount.min, LikesCount.max),
+  comments: Array.from({ length: getRandomNumber(1, COMMENT_COUNT)}, createComment),
 });
 
 const similarPictures = Array.from({ length: PICTURE_COUNT}, createPicture);
